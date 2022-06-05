e63d41470b1eeacc4f83a9985a488ab13f7c932e
jeremycostanzo <jeremy.costanzo@mines-paristech.fr>
2022-06-05 23:30:27 +0200

    refactor: remove comments in main

--- src/main.rs
+++ src/main.rs
@@ -26,20 +26,6 @@ async fn main() -> Result<()> {
     buffer.render(&mut screen)?;
 
     handle_input(&mut buffer, &mut screen).await?;
-    // sleep(Duration::from_millis(2000)).await;
-
-    // let mut in_buffer = [0; 1];
-    // let mut reader = io::stdin();
-    // reader.read_exact(&mut in_buffer)?;
-    // let in_buffer = std::str::from_utf8(&in_buffer)?;
-    // println!("{in_buffer}");
-
-    // sleep(Duration::from_millis(20000)).await;
-
-    // let file = Path::new("src/ui.rs");
-    // let buffer = Buffer::from_file(file).await;
-    // buffer.render(&mut screen)?;
-    // sleep(Duration::from_millis(2000)).await;
 
     Ok(())
 }
