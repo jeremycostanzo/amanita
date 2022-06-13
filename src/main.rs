8e77032330c11ad02161c61872e550a84cd62504
jeremycostanzo <jeremy.costanzo@mines-paristech.fr>
2022-06-13 20:31:05 +0200

    feat: properly print panic messages

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use amanita::buffer::Buffer;
 use amanita::input::handle_input;
 use amanita::EditorBuilder;
 use crossterm::cursor;
+use crossterm::execute;
+use crossterm::terminal;
 use crossterm::QueueableCommand;
 use std::env;
 use std::io::stdout;
@@ -11,9 +13,27 @@ use std::io::Write;
 use anyhow::Result;
 
 use std::path::Path;
+fn cleanup_terminal() {
+    let mut stdout = stdout();
+
+    execute!(stdout, terminal::LeaveAlternateScreen).unwrap();
+    execute!(stdout, cursor::Show).unwrap();
+
+    terminal::disable_raw_mode().unwrap();
+}
+
+// We need to catch panics since we need to close the UI and cleanup the terminal before logging any
+// error messages to the screen.
+fn setup_panic_hook() {
+    std::panic::set_hook(Box::new(|panic_info| {
+        cleanup_terminal();
+        println!("{panic_info}");
+    }));
+}
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    setup_panic_hook();
     let args: Vec<String> = env::args().collect();
     let file_name = &args[1];
 
